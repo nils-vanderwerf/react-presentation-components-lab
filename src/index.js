f3a1d57601125f994592edbadfa3b309441a7342
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-04-12 20:18:54 +1000

    Happy sad toggle

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SimpleComponent from './components/SimpleComponent';
-import SimplerComponent from './components/SimplerComponent';
+import SimplerComponent from './components/SimplerComponent'
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() =>alert("Clicked!")}/>
   </div>,
   document.getElementById('root')
 );
